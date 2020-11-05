c480fb3a23e08476295cb11cb002d4229f8e14ca
AronT <aron.thompson@racunarstvo.hr>
2020-11-05 15:39:05 +0100

    Added base character and abilites scripts

---
+++ Assets/Scripts/PlayerScripts/Abilities.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MetroidvaniaTools
+{
+    public class Abilities : Character
+    {
+        protected Character character;
+
+        protected override void Initializiation()
+        {
+
+
+            base.Initializiation();
+
+            character = GetComponent<Character>();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/Character.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MetroidvaniaTools
+{
+    public class Character : MonoBehaviour
+    {
+        protected Collider2D col;
+        protected Rigidbody2D rb;
+
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Initializiation();
+        }
+
+        protected virtual void Initializiation()
+        {
+            col = GetComponent<Collider2D>();
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        
+    }
+}
