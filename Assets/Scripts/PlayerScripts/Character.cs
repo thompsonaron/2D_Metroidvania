755271f4d15f5e3f717dbf6ff90394fbf9b8aec2
AronT <aron.thompson@racunarstvo.hr>
2020-11-05 19:56:22 +0100

    Implemented sprint

--- Assets/Scripts/PlayerScripts/Character.cs
+++ Assets/Scripts/PlayerScripts/Character.cs
@@ -7,8 +7,13 @@ namespace MetroidvaniaTools
 {
     public class Character : MonoBehaviour
     {
+        [HideInInspector]
+        public bool isFacingLeft;
+
         protected Collider2D col;
         protected Rigidbody2D rb;
+
+        private Vector2 facingLeft;
 
 
 
@@ -22,8 +27,20 @@ namespace MetroidvaniaTools
         {
             col = GetComponent<Collider2D>();
             rb = GetComponent<Rigidbody2D>();
+            facingLeft = new Vector2(-transform.localScale.x, transform.localScale.y);
         }
 
-        
+        protected virtual void Flip()
+        {
+            isFacingLeft = !isFacingLeft;
+            if (isFacingLeft)
+            {
+                transform.localScale = facingLeft;
+            }
+            else
+            {
+                transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/PlayerScripts/HorizontalMovement.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MetroidvaniaTools
+{
+
+    public class HorizontalMovement : Abilities
+    {
+        [SerializeField] protected float timeTillMaxSpeed;
+        [SerializeField] protected float maxSpeed;
+        [SerializeField] protected float sprintMultiplier;
+        private float acceleration;
+        private float currentSpeed;
+        private float horizontalInput;
+        private float runTime;
+
+
+        protected override void Initializiation()
+        {
+
+            base.Initializiation();
+        }
+
+        // Update is called once per frame
+        protected virtual void Update()
+        {
+            MovementPressed();
+            SprintingHeld();
+        }
+
+        protected virtual bool MovementPressed()
+        {
+            float _horizontalInput = Input.GetAxis("Horizontal");
+            if (_horizontalInput != 0)
+            {
+                horizontalInput = _horizontalInput;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        protected virtual bool SprintingHeld()
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            Movement();
+        }
+
+        protected virtual void Movement()
+        {
+            if (MovementPressed())
+            {
+                acceleration = maxSpeed / timeTillMaxSpeed;
+                runTime += Time.deltaTime;
+                currentSpeed = horizontalInput * acceleration * runTime;
+                CheckDirection();
+                SpeedMultiplier();
+            }
+            else
+            {
+                acceleration = 0;
+                runTime = 0;
+                currentSpeed = 0;
+            }
+
+            rb.velocity = new Vector2(currentSpeed, rb.velocity.y);
+        }
+
+        protected void CheckDirection()
+        {
+            if (currentSpeed > 0)
+            {
+                if (character.isFacingLeft)
+                {
+                    //character.isFacingLeft = false;
+                    Flip();
+                }
+
+                if (currentSpeed > maxSpeed)
+                {
+                    currentSpeed = maxSpeed;
+                }
+            }
+            if (currentSpeed < 0)
+            {
+                if (!character.isFacingLeft)
+                {
+                    //character.isFacingLeft = true;
+                    Flip();
+                }
+                if (currentSpeed < -maxSpeed)
+                {
+                    currentSpeed = -maxSpeed;
+                }
+            }
+            
+        }
+
+        protected virtual void SpeedMultiplier()
+        {
+            
+            if (SprintingHeld())
+            {
+                currentSpeed *= sprintMultiplier;
+            }
+        }
+    }
+}
